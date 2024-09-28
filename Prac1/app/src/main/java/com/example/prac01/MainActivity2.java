75a6a0afdd9039be28c311b902649edabd10508d
MKQ <khuatquang1807@gmail.com>
2024-09-28 16:09:59 +0700

    TH2

---
+++ Prac1/app/src/main/java/com/example/prac01/MainActivity2.java
@@ -0,0 +1,50 @@
+package com.example.prac01;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    EditText edtName, edtGPA;
+    Button btnSub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        edtName = findViewById(R.id.edtName);
+        edtGPA = findViewById(R.id.edtGPA);
+        btnSub = findViewById(R.id.btnSub);
+
+        btnSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(MainActivity2.this, MainActivity.class);
+                intent2.putExtra("name", edtName.getText().toString().trim());
+                intent2.putExtra("gpa", edtGPA.getText().toString().trim());
+//                startActivityForResult(intent2, 1);
+                setResult(RESULT_OK, intent2);
+
+                // Finish MainActivity2 and return to MainActivity
+                finish();
+            }
+        });
+
+    }
+}

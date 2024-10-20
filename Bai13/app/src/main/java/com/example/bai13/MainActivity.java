1ac3d64e538be484e943b28e2625ec9f9918f646
MKQ <khuatquang1807@gmail.com>
2024-10-21 00:50:05 +0700

    TH lab

--- Bai13/app/src/main/java/com/example/bai13/MainActivity.java
+++ Bai13/app/src/main/java/com/example/bai13/MainActivity.java
@@ -8,11 +8,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TabHost;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
